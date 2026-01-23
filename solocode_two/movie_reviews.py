8f4adc91f65c317ae176a4305d578dc19c06a48f
tward-albright <taylan.ward001@albright.edu>
2026-01-23 12:54:30 -0500

    worked on movie reviews

---
+++ solocode_two/movie_reviews.py
@@ -0,0 +1,66 @@
+from random import choice
+
+
+class Review:
+    def __init__(self, rating, review):
+        self.rating = self.set_rating(rating)
+        self.review = review
+
+    def set_rating(self, rating):
+        assert rating > 0 and rating <= 5  # change this later to a better error message
+        return rating
+
+    def get_rating(self):
+        return self.rating
+
+    def get_review(self):
+        return self.review
+
+    def display(self):
+        print(f"{self.rating}-star review: \n\t{self.review}")
+
+
+class Movie:
+    def __init__(self, title):
+        self.title = title
+        self.reviews: list[Review] = []
+
+    def add_review(self, review):
+        self.reviews.append(review)
+
+    def get_average_rating(self):
+        scores = [review.get_rating() for review in self.reviews]
+
+        return sum(scores) / len(scores)
+
+    def get_best_review(self):
+        top_reviews = []
+        sorted_reviews = sorted(
+            self.reviews, key=lambda a: a.get_rating(), reverse=True
+        )  # sort by rating
+        top_review_score = sorted_reviews[0].get_rating()
+        for review in sorted_reviews:
+            if review.get_rating() == top_review_score:
+                top_reviews.append(review)
+
+        return choice(top_reviews)
+
+    def get_worst_review(self):
+        top_reviews = []
+        sorted_reviews = sorted(
+            self.reviews, key=lambda a: a.get_rating()
+        )  # sort by rating
+        bottom_review_score = sorted_reviews[0].get_rating()
+        for review in sorted_reviews:
+            if review.get_rating() == bottom_review_score:
+                top_reviews.append(review)
+
+        return choice(top_reviews)
+
+
+def main():
+    # Write a review of Star Wars
+    star_wars = Movie("Star Wars")
+    star_wars.add_review(Review(4.5, "Pretty good! I recommend it!"))
+    star_wars.add_review(Review(2.5, "Eh. Skip it."))
+    star_wars.add_review(Review(3, "It's okay, I guess."))
